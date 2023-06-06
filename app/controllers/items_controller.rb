1c28c5a86ad55dfe95d19a736845e371de6715c5
kiatklai <likhitthaworn.k@gmail.com>
2023-06-06 20:36:12 +0900

    コントローラーで出品機能/画像の投稿追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :item_condition_id, :delivery_fee_id, :area_id, :deliver_day_id, :category_id, :image).merge(user_id: current_user.id)
   end
 end
