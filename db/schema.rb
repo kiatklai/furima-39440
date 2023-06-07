c21d226cedf5064b62435649ab0614fffc90a79e
kiatklai <likhitthaworn.k@gmail.com>
2023-06-07 15:57:47 +0900

    データベース名修正

--- db/schema.rb
+++ db/schema.rb
@@ -48,12 +48,6 @@ ActiveRecord::Schema.define(version: 2023_06_06_101659) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
