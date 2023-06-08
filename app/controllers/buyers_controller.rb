637acf6f3d29ab250a5a4df9c72e6f59b86094ac
kiatklai <likhitthaworn.k@gmail.com>
2023-06-08 19:04:57 +0900

    コントローラーにアクションを設定

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,9 +1,24 @@
 class BuyersController < ApplicationController
-  def index
+  before_action :authenticate_user!, except: :index
 
+  def index
+    @buyer_destination = BuyerDestination.new
   end
 
   def create
+    @buyer_destination = BuyerDestination.new(buyer_params)
+    if @buyer_destination.valid?
+      #pay_item  後日作るpayment method
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
 
+  private
+  def buyer_params
+    # この時点では、order_idが不要。またrequire外の情報は参照するため、mergeとする。
+    params.require(:buyer_destination).permit(:post_code, :area_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
