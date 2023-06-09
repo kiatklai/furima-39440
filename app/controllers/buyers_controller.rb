26a39d252780b594ba3f2ee88f80e83def46500a
kiatklai <likhitthaworn.k@gmail.com>
2023-06-09 20:35:38 +0900

    JSでtoken作成機能追加

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,6 +1,6 @@
 class BuyersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @item = Item.find(params[:item_id])
     @buyer_destination = BuyerDestination.new
@@ -10,8 +10,8 @@ class BuyersController < ApplicationController
     @item = Item.find(params[:item_id])
     @buyer_destination = BuyerDestination.new(buyer_params)
     if @buyer_destination.valid?
-      #pay_item  後日作るpayment method
-      @order_form.save
+      pay_item
+      @buyer_destination.save
       redirect_to root_path
     else
       render :index
@@ -22,5 +22,14 @@ class BuyersController < ApplicationController
   def buyer_params
     # この時点では、order_idが不要。またrequire外の情報は参照するため、mergeとする。
     params.require(:buyer_destination).permit(:post_code, :area_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: buyer_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
